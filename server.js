31e2843e0a5d7e3b079e3d7f340802c6eb26d8ec
UDATHA-DHEERAJ-SAGAR <dheerajudatha2004@gmail.com>
2025-11-29 15:25:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(https://trip-planner-frontend-vtah.onrender.com));
 app.use(express.json());
 
 // Routes
